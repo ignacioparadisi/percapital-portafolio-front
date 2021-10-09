253c604a8b041ae6d24c45606462e9d1424aad6b
Ignacio Paradisi <ignacioparadisi@gmail.com>
2021-10-09 14:24:45 -0500

    Git commit read title values from input

--- src/app/title-form/title-form.component.ts
+++ src/app/title-form/title-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StockTitle } from 'src/common/classes/StockTitle';
 import { StockTitleService } from 'src/services/stock-title/stock-title.service';
 import { MatDialogRef } from '@angular/material/dialog';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-title-form',
@@ -10,13 +11,31 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class TitleFormComponent implements OnInit {
 
-  constructor(private stockTitleService: StockTitleService, private dialogRef: MatDialogRef<TitleFormComponent>) { }
+  titleForm: FormGroup;
+  public validationMessages = {
+    name: [],
+    symbol: []
+  };
+
+  constructor(private stockTitleService: StockTitleService, private dialogRef: MatDialogRef<TitleFormComponent>) { 
+  }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
   submit() {
-    let stockTitle = new StockTitle('Nombre', 'NBR');
+    this.validateNameField();
+    this.validateSymbolField();
+    if (!this.titleForm.valid) {
+      return;
+    }
+    let name = this.titleForm.get('name')?.value;
+    let symbol = this.titleForm.get('symbol')?.value;
+    if (!name || !symbol) {
+      throw Error('Required fields');
+    }
+    let stockTitle = new StockTitle(name, symbol);
     this.stockTitleService.createTitle(stockTitle).subscribe((title: StockTitle) => {
       console.info('Did create Stock Title', title);
       this.dismiss();
@@ -25,6 +44,42 @@ export class TitleFormComponent implements OnInit {
 
   dismiss() {
     this.dialogRef.close();
+  }
+
+  private createForm() {
+    this.titleForm = new FormGroup({
+      name: new FormControl({ value: '', disabled: false }, [Validators.required]),
+      symbol: new FormControl({ value: '', disabled: false }, [Validators.required])
+    })
+  }
+
+  /**
+   * Valida la información que contiene el campo de email.
+   */
+   private validateNameField() {
+    this.validationMessages.name = [];
+    const nameErrors = this.titleForm.get('name')?.errors;
+    console.log(nameErrors);
+    if (nameErrors) {
+      if (nameErrors.required) {
+        // @ts-ignore
+        this.validationMessages.name.push('El nombre es obligatorio');
+      }
+    }
+  }
+
+  /**
+   * Valida la información que contiene el campo de contraseña.
+   */
+  private validateSymbolField() {
+    this.validationMessages.symbol = [];
+    const symbolErrors = this.titleForm.get('symbol')?.errors;
+    if (symbolErrors) {
+      if (symbolErrors.required) {
+        // @ts-ignore
+        this.validationMessages.symbol.push('El simbolo es obligatorio.');
+      }
+    }
   }
 
 }
