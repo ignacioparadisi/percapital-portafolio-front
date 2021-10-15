5f78a93ba3d03f52c7f38471efec362625ab084c
Ignacio Paradisi <ignacioparadisi@gmail.com>
2021-10-15 00:11:50 -0500

    Refactor GraphQLQuery and GraphQLMutation

--- src/services/stock-title/stock-title.service.ts
+++ src/services/stock-title/stock-title.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { StockTitle } from 'src/common/classes/StockTitle';
-import { Apollo } from 'apollo-angular';
 import { MutationFactory } from '../apollo/mutations/MutationFactory';
 import { QueryFactory } from '../apollo/queries/QueryFactory';
 import { Page } from 'src/common/classes/Page';
