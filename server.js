186264fb0c211dae38730d9fda7335147aacf0c1
Ignacio Paradisi <ignacioparadisi@gmail.com>
2021-10-22 20:09:48 -0500

    Change dist name

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 app = express();
 
-app.use(express.static('./dist/percapital-portfolio-front'));
+app.use(express.static('./dist/percapital-portafolio-front'));
 
 app.get('/*', (req, res) => {
-    res.sendFile('index.html', { root: 'dist/percapital-portfolio-front/'});
+    res.sendFile('index.html', { root: 'dist/percapital-portafolio-front/'});
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
