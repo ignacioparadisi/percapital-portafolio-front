e4dce4f8f9115775e2a263a6c434cc43f3a316a0
Ignacio Paradisi <ignacioparadisi@gmail.com>
2022-03-28 18:23:33 -0600

    Build for prod

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
+import {isDevMode, NgModule} from '@angular/core';
 import {Apollo, ApolloModule} from 'apollo-angular';
 import { ApolloClientOptions, InMemoryCache, ApolloLink } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
 import { setContext } from '@apollo/client/link/context';
 
 const isBackendRunningLocally = true;
-const serverURL = isBackendRunningLocally ? 'http://localhost:4000' : 'https://percapital-backend.herokuapp.com';
-const predictionURL = 'http://localhost:4001';
+const serverURL = isDevMode() ? 'http://localhost:4000' : 'https://percapital-backend.herokuapp.com';
+const predictionURL = isDevMode() ? 'http://localhost:4001' : 'https://percapital-backend-prediction.herokuapp.com';
 
 function createApollo(httpLink: HttpLink, uri: string): ApolloClientOptions<any> {
   const auth = setContext(() => ({
