55fec338aae91e3ec106f1d6a7cccd9c5db4c80a
Ignacio Paradisi <ignacioparadisi@gmail.com>
2022-10-14 19:21:54 -0500

    Download reports csv

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -23,7 +23,7 @@ export class ReportsComponent implements OnInit {
     'sellQuotationChange'
   ]
   reportsDataSource = new MatTableDataSource<Report>(this.reports);
-  
+
 
   constructor(private predictionService: PredictionService, private reportsService: ReportsService) { }
 
@@ -78,6 +78,60 @@ export class ReportsComponent implements OnInit {
 
   abs(value: number) {
     return Math.abs(value);
+  }
+
+  download() {
+    this.downloadFile(this.reports);
+  }
+
+  downloadFile(data: any, filename = 'data') {
+    let headerTitles = {
+      'symbol': 'Simbolo',
+      'latestPrice': 'Último Precio',
+      'changePercentage': 'Subida/Bajada de Acción',
+      'buyChangePercentage': 'Subida/Bajada de Compra',
+      'sellChangePercentage': 'Subida/Bajada de Venta'
+    }
+    let csvData = this.ConvertToCSV(data, headerTitles, ['symbol',
+      'latestPrice',
+      'changePercentage',
+      'buyChangePercentage',
+      'sellChangePercentage']);
+    console.log(csvData)
+    let blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
+    let dwldLink = document.createElement("a");
+    let url = URL.createObjectURL(blob);
+    let isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
+    if (isSafariBrowser) {  //if Safari open in new window to save file with random filename.
+      dwldLink.setAttribute("target", "_blank");
+    }
+    dwldLink.setAttribute("href", url);
+    dwldLink.setAttribute("download", filename + ".csv");
+    dwldLink.style.visibility = "hidden";
+    document.body.appendChild(dwldLink);
+    dwldLink.click();
+    document.body.removeChild(dwldLink);
+  }
+  ConvertToCSV(objArray: any, headerTitles: any, headerList: string[]) {
+    let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    let str = '';
+    let row = 'S.No,';
+    for (let index in headerList) {
+      let head = headerList[index];
+      row += headerTitles[head] + ',';
+    }
+    row = row.slice(0, -1);
+    str += row + '\r\n';
+    for (let i = 0; i < array.length; i++) {
+      let line = (i + 1) + '';
+      for (let index in headerList) {
+        let head = headerList[index];
+        line += ',' + array[i][head];
+      }
+      line = line.replace(/null/g, '0');
+      str += line + '\r\n';
+    }
+    return str;
   }
 
 }
